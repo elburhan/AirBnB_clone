dd5aca46ab46be5b4123fddbbad52ec64e1725bd
Blessed Oigbochie <eprince1800@gmail.com>
2023-12-07 18:03:32 +0100

    all classes in place

---
+++ models/place.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Defines the Place class.
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """
+    Represents the place class for the HBnB project
+    """
+    city_id = ""  # empty string
+    user_id = ""  # empty string
+    name = ""  # empty string
+    description = ""  # empty string
+    number_rooms = 0  # integer - 0
+    number_bathrooms = 0  # integer - 0
+    max_guest = 0  # integer - 0
+    price_by_night = 0  # integer - 0
+    latitude = 0.0  # float - 0.0
+    longitude = 0.0  # float - 0.0
+    amenity_ids = []  # list of string - empty list
---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines the State class.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Represents a State for the HBnB project.
+    """
+    
+    name = ""  # empty string
