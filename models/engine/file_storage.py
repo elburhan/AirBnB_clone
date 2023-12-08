d029da622172a25de6246841054266342d46c0a7
Oigbochie Blessed <eprince1800@gmail.com>
2023-12-08 06:33:01 +0100

    moving to main

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -88,17 +88,6 @@ class FileStorage:
     def attributes(self):
         """Returns the valid attributes and their types for classname"""
         attributes = {
-<<<<<<< HEAD
-            "BaseModel": {"id": str, "created_at": datetime.datetime, "updated_at": datetime.datetime},
-            "User": {"email": str, "password": str, "first_name": str, "last_name": str},
-            "State": {"name": str},
-            "City": {"state_id": str, "name": str},
-            "Amenity": {"name": str},
-            "Place": {"city_id": str, "user_id": str, "name": str, "description": str,
-                      "number_rooms": int, "number_bathrooms": int, "max_guest": int,
-                      "price_by_night": int, "latitude": float, "longitude": float, "amenity_ids": list},
-            "Review": {"place_id": str, "user_id": str, "text": str}
-=======
             "BaseModel":
                      {"id": str,
                       "created_at": datetime.datetime,
@@ -131,6 +120,5 @@ class FileStorage:
             {"place_id": str,
                          "user_id": str,
                          "text": str}
->>>>>>> main
         }
         return attributes
